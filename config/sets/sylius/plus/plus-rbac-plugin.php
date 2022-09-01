e90a6fb1476aef4d0e5b27c567e6dc13c25e9236
Adam Kasperczak <adamkasperczak123@gmail.com>
2022-09-01 14:41:53 +0200

    initialise trait in constructor

--- config/sets/sylius/plus/plus-rbac-plugin.php
+++ config/sets/sylius/plus/plus-rbac-plugin.php
@@ -2,9 +2,13 @@
 
 declare(strict_types=1);
 
+use PhpParser\Node\Stmt\Class_;
 use Rector\Config\RectorConfig;
 use Sylius\SyliusRector\Rector\Class_\AddInterfaceToClassExtendingTypeRector;
+use Sylius\SyliusRector\Rector\Class_\AddMethodCallToConstructorForClassesUsingTraitRector;
 use Sylius\SyliusRector\Rector\Class_\AddTraitToClassExtendingTypeRector;
+use Sylius\SyliusRector\Rector\Dto\AddMethodCallToConstructorForClassesUsingTrait;
+use Sylius\SyliusRector\Rector\TraitUse\AliasTraitMethodRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->ruleWithConfiguration(AddInterfaceToClassExtendingTypeRector::class, [
@@ -17,6 +21,22 @@ return static function (RectorConfig $rectorConfig): void {
         'Sylius\Component\Core\Model\AdminUser' => [
             'Sylius\PlusRbacPlugin\Domain\Model\ToggleablePermissionCheckerTrait',
             'Sylius\PlusRbacPlugin\Domain\Model\RoleableTrait',
+        ],
+    ]);
+
+    $rectorConfig->ruleWithConfiguration(AliasTraitMethodRector::class, [
+        'Sylius\PlusRbacPlugin\Domain\Model\RoleableTrait' => [
+            [
+                'traitMethod' => '__construct',
+                'newMethodName' => 'initializeRoleableTrait',
+                'visibility' => Class_::MODIFIER_PRIVATE,
+            ],
+        ],
+    ]);
+
+    $rectorConfig->ruleWithConfiguration(AddMethodCallToConstructorForClassesUsingTraitRector::class, [
+        'Sylius\PlusRbacPlugin\Domain\Model\RoleableTrait' => [
+            new AddMethodCallToConstructorForClassesUsingTrait('this', 'initializeRoleableTrait'),
         ],
     ]);
 };
---
+++ stubs/Sylius/PlusRbacPlugin/Domain/Model/RoleableTrait.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sylius\PlusRbacPlugin\Domain\Model;
+
+if (trait_exists('Sylius\PlusRbacPlugin\Domain\Model\RoleableTrait')) {
+    return;
+}
+
+trait RoleableTrait
+{
+}

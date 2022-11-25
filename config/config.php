632440df4b66112f70dd324e5e759749b8259d88
Rafikooo <thecrazyrafik@gmail.com>
2022-11-25 08:55:24 +0100

    Include static analysis in CI

--- config/config.php
+++ config/config.php
@@ -6,11 +6,12 @@ use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
     $services = $rectorConfig->services();
-    $services->defaults()
-        ->public()
-        ->autowire()
-        ->autoconfigure();
+    $configurator = $services->defaults();
 
-    $services->load('Sylius\\SyliusRector\\', __DIR__ . '/../src')
-        ->exclude([__DIR__ . '/../src/Rector', __DIR__ . '/../src/ValueObject', __DIR__ . '/../src/PhpDoc/Node']);
+    $configurator->public();
+    $configurator->autowire();
+    $configurator->autoconfigure();
+
+    $prototypeConfigurator = $services->load('Sylius\\SyliusRector\\', __DIR__ . '/../src');
+    $prototypeConfigurator->exclude([__DIR__ . '/../src/Rector', __DIR__ . '/../src/ValueObject', __DIR__ . '/../src/PhpDoc/Node']);
 };

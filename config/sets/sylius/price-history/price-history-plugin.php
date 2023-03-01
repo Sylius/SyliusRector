a135edc4aa68f0a5ad1af280762c0b98976e9f8a
Rafikooo <thecrazyrafik@gmail.com>
2023-03-01 19:08:00 +0100

    Add PriceHistoryPlugin rules

---
+++ config/sets/sylius/price-history/price-history-plugin.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Sylius\SyliusRector\Rector\Class_\AddInterfaceToClassExtendingTypeRector;
+use Sylius\SyliusRector\Rector\Class_\AddTraitToClassExtendingTypeRector;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->ruleWithConfiguration(AddInterfaceToClassExtendingTypeRector::class, [
+        'Sylius\Component\Core\Model\Channel' => [
+            'Sylius\PriceHistoryPlugin\Domain\Model\ChannelInterface',
+        ],
+        'Sylius\Component\Core\Model\ChannelPricing' => [
+            'Sylius\PriceHistoryPlugin\Domain\Model\ChannelPricingInterface',
+        ],
+    ]);
+    $rectorConfig->ruleWithConfiguration(AddTraitToClassExtendingTypeRector::class, [
+        'Sylius\Component\Core\Model\Channel' => [
+            'Sylius\PriceHistoryPlugin\Domain\Model\LowestPriceForDiscountedProductsAwareTrait',
+        ],
+        'Sylius\Component\Core\Model\ChannelPricing' => [
+            'Sylius\PriceHistoryPlugin\Domain\Model\LowestPriceBeforeDiscountAwareTrait',
+        ],
+    ]);
+};

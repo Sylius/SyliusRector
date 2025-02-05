856cb64a0fe8bb25348e14673ad8f44b36e018ba
Rafikooo <thecrazyrafik@gmail.com>
2025-02-05 09:57:30 +0100

    Add repository and core model stubs

---
+++ stubs/Sylius/Bundle/ChannelBundle/Doctrine/ORM/ChannelRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sylius\Bundle\ChannelBundle\Doctrine\ORM;
+
+if (class_exists('Sylius\Bundle\ChannelBundle\Doctrine\ORM\ChannelRepository')) {
+    return;
+}
+
+class ChannelRepository
+{
+}
---
+++ stubs/Sylius/Bundle/TaxonomyBundle/Doctrine/ORM/TaxonRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Sylius\Bundle\TaxonomyBundle\Doctrine\ORM;
+
+if (class_exists('Sylius\Bundle\TaxonomyBundle\Doctrine\ORM\TaxonRepository')) {
+    return;
+}
+
+class TaxonRepository
+{
+}

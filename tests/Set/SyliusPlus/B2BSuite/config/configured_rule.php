8db6ef0c14d72f0595e80c10812e29da30deccc8
Wojdylak <karolwojdyla37@gmail.com>
2025-05-06 12:22:42 +0200

    [B2B] Add rules for version 2.1

--- tests/Set/SyliusPlus/B2BSuite/config/configured_rule.php
+++ tests/Set/SyliusPlus/B2BSuite/config/configured_rule.php
@@ -7,5 +7,8 @@ use Sylius\SyliusRector\Set\SyliusPlus;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(__DIR__ . '/../../../../../config/config.php');
-    $rectorConfig->sets([SyliusPlus::B2B_SUITE]);
+    $rectorConfig->sets([
+        SyliusPlus::B2B_SUITE_20,
+        SyliusPlus::B2B_SUITE_21,
+    ]);
 };

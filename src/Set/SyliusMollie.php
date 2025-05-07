ef83ce117dc0706f3d650cd39b85a38c7dd11471
TheMilek <kam.grygierzec@gmail.com>
2025-05-07 11:59:05 +0200

    Add rector for disabling MolliePlugin

---
+++ src/Set/SyliusMollie.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\SyliusRector\Set;
+
+final class SyliusMollie
+{
+    public const REMOVE_MOLLIE_PLUGIN_FROM_SYLIUS_114 = __DIR__ . '/../../config/sets/sylius/mollie-plugin/remove-mollie-plugin-from-sylius-1.14.php';
+}
---
+++ stubs/Sylius/MolliePlugin/Entity/OnboardingStatusAwareInterface.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Sylius\MolliePlugin\Entity;
+
+if (class_exists('Sylius\MolliePlugin\Entity\OnboardingStatusAwareInterface')) {
+    return;
+}
+
+interface OnboardingStatusAwareInterface
+{
+}

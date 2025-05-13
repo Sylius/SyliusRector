f4792ec43107e1def596cf22bbfbd5edea2d6d44
Michał Pysiak <mpysiak@gmail.com>
2025-05-13 10:06:42 +0200

    Add rector for RfqPlugin

--- src/Set/SyliusPlus.php
+++ src/Set/SyliusPlus.php
@@ -23,4 +23,6 @@ final class SyliusPlus
     public const RETURN_PLUGIN = __DIR__ . '/../../config/sets/sylius/plus/return-plugin.php';
 
     public const UPGRADE_TO_MODULAR = __DIR__ . '/../../config/sets/sylius/plus/up-to-plus-modular.php';
+
+    public const RFQ_PLUGIN = __DIR__ . '/../../config/sets/sylius/plus/rfq-plugin.php';
 }
---
+++ stubs/Sylius/RfqPlugin/Entity/Order/OrderItemInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+
+if (class_exists('Sylius\RfqPlugin\Entity\Order\OrderItemInterface')) {
+    return;
+}
+
+interface OrderItemInterface
+{
+}

81783cc9891f503e9e0754808d39a5b8ac8fcf20
Jan Goralski <jan.wojciech.goralski@gmail.com>
2025-09-26 12:22:52 +0200

    [AdyenPlugin] Add rules for version 2.0

--- src/Set/SyliusAdyen.php
+++ src/Set/SyliusAdyen.php
@@ -7,4 +7,5 @@ namespace Sylius\SyliusRector\Set;
 final class SyliusAdyen
 {
     public const ADYEN_PLUGIN_10 = __DIR__ . '/../../config/sets/sylius/adyen-plugin/adyen-plugin-1.0.php';
+    public const ADYEN_PLUGIN_20 = __DIR__ . '/../../config/sets/sylius/adyen-plugin/adyen-plugin-2.0.php';
 }
---
+++ tests/Set/AdyenPlugin/config/adyen_plugin_2_0.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+use Sylius\SyliusRector\Set\SyliusAdyen;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->import(__DIR__ . '/../../../../config/config.php');
+    $rectorConfig->sets([SyliusAdyen::ADYEN_PLUGIN_20]);
+};

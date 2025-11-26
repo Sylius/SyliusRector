eac35bf9d67347a25a00139f2863dc3822639b77
Rafikooo <thecrazyrafik@gmail.com>
2025-11-26 16:39:52 +0100

    Add SetProvider entries for B2B Kit 3.0 and Elasticsearch Plugin 5.2
    Remove deprecated B2B_SUITE_30 constant and set file in favor of composer-triggered set. Add BitBag Elasticsearch Plugin 5.2 support alongside B2B Kit 3.0 for automated rector rule loading during package installation.

--- src/SetProvider/SyliusSetProvider.php
+++ src/SetProvider/SyliusSetProvider.php
@@ -13,6 +13,19 @@ final class SyliusSetProvider implements SetProviderInterface
     public function provide(): array
     {
         return [
+            new ComposerTriggeredSet(
+                'symfony',
+                'bitbag/elasticsearch-plugin',
+                '5.2',
+                __DIR__ . '/../../config/sets/bitbag/elasticsearch-plugin/elasticsearch-plugin-5.2.php'
+            ),
+
+            new ComposerTriggeredSet(
+                'symfony',
+                'sylius/b2b-kit',
+                '3.0',
+                __DIR__ . '/../../config/sets/sylius/b2b-kit/b2b-kit-3.0.php'
+            ),
         ];
     }
 }
